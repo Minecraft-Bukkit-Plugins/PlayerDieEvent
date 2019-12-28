ab445b31e583e3c02a6234a9c605f256f0f10c32
Franco <franco.mato.contin@gmail.com>
2019-12-28 19:08:37 -0300

    Fixed when player die chest was placed but the action was the same for all chests

--- src/me/franco28/playereventdie/die.java
+++ src/me/franco28/playereventdie/die.java
@@ -1,5 +1,6 @@
 package me.franco28.playereventdie;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.bukkit.Bukkit;
@@ -17,18 +18,55 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.fusesource.jansi.Ansi;
 
 public class die extends JavaPlugin implements Listener {
 
+    ArrayList<String> chesty = new ArrayList<String>();
+    List<String> chests = getConfig().getStringList("chests");
+    final static List<Material> types = Arrays.asList(Material.CHEST, Material.DISPENSER, Material.TRAPPED_CHEST, Material.FURNACE,
+            Material.DROPPER, Material.ENCHANTING_TABLE, Material.HOPPER);
+    
 	public void onEnable() {
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin Cofre Activado!");
 		getServer().getPluginManager().registerEvents(this, this);
+		
+	        getConfig().set("chests", chests);
+	        saveConfig();
+	        for (String str : chests) {
+	            String[] words = str.split(",");
+	            String aWorld = words[0];
+	            String aX = words[3];
+	            double aX1 = Double.parseDouble(aX);
+	            String aY = words[4];
+	            double aY1 = Double.parseDouble(aY);
+	            String aZ = words[5];
+	            double aZ1 = Double.parseDouble(aZ);
+	            Location chestLoc = new Location (Bukkit.getWorld(aWorld), aX1, aY1, aZ1);
+	           
+	            if (chestLoc.getBlock().getType() != Material.CHEST) {
+	                chestLoc.getBlock().setType(Material.CHEST);
+	                   ItemStack lc = new ItemStack(Material.CHEST);
+	                   ItemMeta meta = lc.getItemMeta();
+	                   meta.setDisplayName(ChatColor.GOLD + ("Cofre de la Muerte"));
+	                   lc.setItemMeta(meta);
+	              
+	            } else {
+	                getLogger().info(Ansi.ansi().fg(Ansi.Color.YELLOW) + "Cofre de la Muerte ya esta en la misma ubicacion " + aX + "," + aY + "," + aZ + "!" + Ansi.ansi().fg(Ansi.Color.DEFAULT));
+	            }
+	        }
 	}
     
 	public void onDisable() {
     	getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Plugin Cofre Desactivado!");
-    }
+    
+    	List<String> chests = getConfig().getStringList("chests");
+        chesty.addAll(chests);
+        getConfig().set("chests", chesty);
+        saveConfig();
+	}
 
 	@EventHandler(priority = EventPriority.HIGH)
     public void PlayerDeath(PlayerDeathEvent event) {
@@ -42,24 +80,32 @@ public class die extends JavaPlugin implements Listener {
         ItemStack[] drops = toItemStack(event.getDrops());
         Chest c = (Chest) loc.getBlock().getState();
         c.getBlockInventory().setContents((ItemStack[])drops);
-        c.setLock(player.getName());        
-        
+        c.setLock(player.getName());                          
+        c.setCustomName(ChatColor.GOLD + "" + ChatColor.BOLD + "Cofre de Muerte de: " + player.getName());
+
         int count = 0;
-         
+       
         for(ItemStack i : drops) {
         if (i.getItemMeta() == null) {
         count++;
         }      
         event.getDrops().remove(count);
         }        
-        
- 	   if(player.isDead()) {
- 		  player.getKiller();
-           if(player.getKiller() instanceof Player) {
-               player.sendMessage(ChatColor.RED + "Mataste a " + player.getName() + "!");
-			    Bukkit.broadcastMessage(ChatColor.RED + player.getName() + "Murió por " + player.getKiller() );
-           }
-       }
+         	   
+       player.getLocation().getWorld().playEffect(player.getLocation(), Effect.SMOKE, 12);
+       player.getLocation().getWorld().playEffect(player.getLocation(), Effect.POTION_BREAK, 12);
+       
+       event.setKeepLevel(true);
+       event.setDroppedExp(0); 
+       player.setTotalExperience(player.getTotalExperience() - (int)((player.getTotalExperience() - event.getNewTotalExp())* 0.75));
+	
+       ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+       BookMeta bm = (BookMeta) book.getItemMeta();
+       bm.setPages(Arrays.asList("Este libro se a encontrado dentro el cofre del jugador " + player.getName() + " por lo cual, si los objectos de este cofre no estan, reportar a un admin." ));
+       bm.setAuthor(player.getName());
+       bm.setTitle("Este cofre pertenece a: " + player.getName());
+       book.setItemMeta(bm);
+       c.getInventory().addItem(book);
 	}
     
     public ItemStack[] toItemStack(List<ItemStack> list) {
@@ -71,26 +117,22 @@ public class die extends JavaPlugin implements Listener {
     	}
 		return items;		
     	}
-
-    @EventHandler(priority = EventPriority.HIGH)
+       	    	
+	@EventHandler(priority = EventPriority.HIGH)
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         Player player = (Player) event.getPlayer();      
         int x = player.getLocation().getBlockX();
         int y = player.getLocation().getBlockY();
         int z = player.getLocation().getBlockZ();
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        ItemStack apple = new ItemStack(Material.ENCHANTED_GOLDEN_APPLE);
         BookMeta bm = (BookMeta) book.getItemMeta();
-        bm.setPages(Arrays.asList(player.getName() + ". Has muerto, por lo cual tus ultimos objetos estan aqui: " + "X: " + x + " Y: " + y + " Z: " + z + ". Apresurate! ya que tu cofre puede ser robado :( "));
-      bm.setAuthor("Server by TatinSystem & sims03");
-      bm.setTitle("Coordenadas de tu ultima muerte!");
-      book.setItemMeta(bm);
-      player.getInventory().addItem(book);
-    }
-               
-    @EventHandler(priority = EventPriority.HIGH)
-    public void onPlayerDeath(PlayerDeathEvent event)
-    {
-        Player player = event.getEntity();
-        player.getLocation().getWorld().playEffect(player.getLocation(), Effect.SMOKE, 1);
-    }
+        bm.setPages(Arrays.asList(player.getName() + ". Has muerto, por lo cual tus ultimos objetos estan aqui: " + "X: " + x + " Y: " + y + " Z: " + z + ". Apresurate! ya que tu cofre puede ser robado :( ", "El server te regala una Manzana Dorada Encantada!" + " PD: El Server"));
+        bm.setAuthor("Server");
+        bm.setTitle("Coordenadas de tu ultima muerte!");
+        book.setItemMeta(bm);
+        player.getInventory().addItem(book);
+        player.getInventory().addItem(apple);
+        player.sendMessage(ChatColor.BOLD + "Bienvenido de la muerte! "+ player.getName());
+    }    
 }
